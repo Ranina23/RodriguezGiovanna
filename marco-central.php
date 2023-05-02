3c564ee1e876ff0842d1cc237b1465aef62cb2b6
GiovannaRodriguez <69163589+GiovannaRodriguez@users.noreply.github.com>
2023-05-01 21:58:09 -0500

    agregar d

--- marco-central.php
+++ marco-central.php
@@ -2,21 +2,21 @@
    <div  style="width: 100%;  ">     
     <div style="width: 100%; height:100%; margin: auto ; display:inline-block;" >
        <div style=" width:100%; height: 100%; top:0; font-size:20px; text-align:center; float:left; ">
-        	<h1>¿Quién es Stalin A. Francis Quinde.?</h1>
+        	<h1>¿Quién es Giovanna S. Rodriguez Ortiz?</h1>
        		 <hr>
              
           <div id="posteo" style="text-align: left; font-size:0.5vw;">
-                Posteado por:  Stalin Francis<br>
-                Fecha: 2023-04-30 <br>
+                Posteado por:  Giovanna Rodríguez<br>
+                Fecha: 2023-05-01 <br>
           </div>
         </div>
     </div>
   </div>
   <div style="width: 100%; height:100%;" >
            <div  style="text-align:justify; font-size: 20px;">
-                   <p class="eys-parrafo">Hola, yo soy Stalin Adalberto <b> Francis Quinde </b>, ingeniero esmeraldeño que labora desde hace 10 años en la Universidad Tecnica Luis Vargas Torres, e impartido diferentes cátedras en la carrera de Tecnología de la Información entre las cuales esta Ingeniería de Software, Programación Web, Proyectos informáticos entre otras; pero la cátedra en la que mejor me desempeño es la de Fundamentos de programación que se dicta en el primer ciclo de la carrera de Tecnología de la Información. </p>
-                   <p class="eys-parrafo">Desde mís estudios secundarios me he apacionado por las ciencias y la tecnología, quieza sería  por esta capacidad que tiene estas dos áres del conocimiento de solucionar muchos probemas de la sociedad; es por eso que desde que termine mis estudios de ingeniería en la ESPOL, me he dedicado a la programación de computadoras. </p>
-<p class="eys-parrafo"> El primer lenguaje con el cual empece a programar en el tercer ciclo de universidad fue el <b><a href="https://en.wikipedia.org/wiki/BASIC"> basic</a></b>  para este tiempo la ESPOL contaba con laboratorios de computadora  una computadora personal de marca IBM de tip XT modelo 5160  en es.</p>
+                   <p class="eys-parrafo">Hola, yo soy Giovanna Sofia <b> Rodríguez Ortiz </b>, soy una estudiante de la Facultad de Ingenierías de la Universidad Tecnica Luis Vargas Torres, sigo la carrera de Ingeniería en Tecnologías de la Información. </p>
+                   <p class="eys-parrafo">Desde mís estudios en el bachillerato, he sentido cierta atracción por todo lo relacionado con la innovación tecnológica (crear programas, aplicaciones...), además de que en cierto modo también he estado muy interesada en la Inteligencia Artificial. </p>
+<p class="eys-parrafo"> Además de la programación en sí, me encanta lo que es Base de Datos y todo lo que tenga que ver con esta materia. Se me ha hecho muy versátil trabajar con el lenguaje que se requiere para esta materia el cual es llamado<b><a href="https://aws.amazon.com/es/what-is/sql/"> Lenguaje SQL</a></b>.</p>
 
    <div id="blog-seccion2"  style="height: 50px;"></div>
 
@@ -24,23 +24,23 @@
     
 
 
-                   <p class="eys-parrafo">Mis estudios secundarios los realice en el colegio Fisco-Misional Sagado Corazon donde me gradue en el año 1989, de la especialidad de Físico-Matemático, apenas me gradue viaje a Guayaquil para seguir mis estudios universitarios en la Escuela Superior Politécnica del Litoral donde seguí la carrera de Ingeniería en Computación, gracias a la demanda que exitía en este tiempo de este tipo de profesionales comencé a trabajar mientra estaba estudiando, prestando mis servicios profesionales en empresas como Poligráfica, El telegráfo, Intecorsorcio; mientras trabajaba en esta última logré obtener mi título de Ingeniero de Computación.</p>
+                   <p class="eys-parrafo">Mis estudios secundarios los realice en la Unidad Educativa Fisco-Misional Salesiana "María Auxiliadora" donde me gradué en el año 2019, de la especialidad de Ciencias-Quibio, apenas me gradué postulé para obtener un cupo en la Universidad Técnica Luis Vargas Torres de Esmeraldas para la carrera de Ingeniería en Tecnologías de la Información, cupo que obtuve y en ese mismo año 2019 empecé el curso de Admisión, el cual aprobé con excelentes calificaciones y finalmente pude seguir mi carrera.</p>
                    <p class="eys-parrafo">Ya con el título en la mano decidi regrasar a mi tierra Esmeraldas, con la finalidad de aplicar los conocimiento y habilidades obtenida por lo que forme la empresa EDUCAYSOFT, pero como los ingreso no fueron suficiente entre a trabajar al colegio Eloy Alfaro y posteiormente a la Universidad Técnica Luis Vargas Torres donde actualmente laboró.</p>
-<p class="eys-parrafo"> Por las exigencias de la universidad, ingresar a la maestria de docencia y desarrollo del curriculo, de la cual de grauduo obteniendo el título de Magister, pero despues realice otra maestría en la POLITECNICA, de Ciencias de la Computación.</p>
+<p class="eys-parrafo"> Espero más adelante poder sacar una maestría en Big Data y desempeñarme en esta disciplina.</p>
    <div id="blog-seccion3"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mi perfil informáticos</h2> 
     
 
 
-                   <p  class="eys-parrafo">Mi perfil tecnológico es la programación de computadoras, mi primer programa lo hice en el leguaje Basic, utilizando el computador de la espol, luego programe c++, donde realiza algunos programa que leina el entregas del puerto serial y enviaban señales a un motor. Profesionalmente comenza programando en Informix un lenguaje de programación para el sistema Unix, utilizando uan versio de foxpro para unix, luego cuando aparecio la interface gráfica programe en foxpro y seguidamente visual foro.</p> 
-                   <p  class="eys-parrafo">Hoy en día creso sisemas web combinando HTML, CSS, JavaScrip y PHP; tambien aplicaciones para dispositivo androi para lo cual utilizo el framework android Studio, el cual utiliza el lenguaje de JAVA.</p>
+                   <p  class="eys-parrafo">Por ahora he estado creando programas pequeños, los cuales han sido creados durante mi carrera universitaria. He creado programas como páginas web, calculadora, entre otros programas pequeños. También hice una aplicación utilizando Android Studio.</p> 
+                   <p  class="eys-parrafo">Desde que empecé en el mundo de la programación, he venido combinando HTML, CSS, JavaScrip y PHP para la creación de la gran mayoría de mis programas; así mismo he venido usando lenguaje SQL.</p>
    <div id="blog-seccion4"  style="height: 50px;"></div>
 
     <h2 style="border-bottom: 1px solid green;"> Mis Hooby</h2> 
     
 
-                   <p   class="eys-parrafo">Soy amante a la lectura, así que en los ratos libres me encanta actualizar mis conocimientos leyendo manuales de los leguajes de progración que namejo, y tambien muchos noticias de las ultimas tendencias en tecnología.</p>
+                   <p   class="eys-parrafo">En mis ratos libres paso en casa leyendo, escuchando música, viendo algún programa o ayudando en las labores de mi casa. También me gusta mucho cocinar.</p>
        
 
                    
