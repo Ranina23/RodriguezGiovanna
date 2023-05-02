d0028a3e9396c04b404cbd52867528659669ceea
GiovannaRodriguez <69163589+GiovannaRodriguez@users.noreply.github.com>
2023-05-01 22:17:07 -0500

    actualizar d

--- foot.php
+++ foot.php
@@ -6,7 +6,6 @@
                             <li><a href="mailto:educaysoft%40gmail.com" class="icon  fa-envelope" title="Email"><span class="label">Capacitación</span></a></li>
                             <li><a href="https://twitter.com/home?lang=es" class="icon  fa-twitter" target="_blank" title="Twitter"><span class="label">Asesoria técnica</span></a></li>
                             <li><a href="https://www.facebook.com/stalin.francis.7/" class="icon  fa-facebook" target="_blank" title="Facebook"><span class="label">Investigación</span></a></li>
-	                    <li><a href="" class="icon  fa-linkedin" target="_blank"  title="LinkedIn"><span class="label">Desarrollo Web</span></a></li>
                     </ul>
             </div>
             <div  style="float:left; width:25%; position:relative; text-align:left;">
@@ -14,7 +13,6 @@
                             <li><a href="mailto:educaysoft%40gmail.com" class="icon  fa-envelope" title="Email"><span class="label">Email</span></a></li>
                             <li><a href="https://twitter.com/home?lang=es" class="icon  fa-twitter" target="_blank" title="Twitter"><span class="label">Twitter</span></a></li>
                             <li><a href="https://www.facebook.com/stalin.francis.7/" class="icon  fa-facebook" target="_blank" title="Facebook"><span class="label">Facebook</span></a></li>
-				                    <li><a href="" class="icon  fa-linkedin" target="_blank"  title="LinkedIn"><span class="label">LinkedIn</span></a></li>
                     </ul>
  
             </div>
@@ -24,7 +22,7 @@
 
             		    <ul style="margin:0; padding:0;" class="copyright">
                             <li><a href="http://educaysoft.org/educaysoft/index.php">EDUCAYSOFT</a></li>
-                            <li>&copy; Francis Quinde Stalin Adalberto--</li>
+                            <li>&copy; Giovanna Sofia Rodríguez Ortiz--</li>
                             <li>Design: <a href="//html5up.net">HTML5 UP</a></li>
                         </ul>
             </div>
--- index.php
+++ index.php
@@ -28,7 +28,7 @@ include("foot.php")
 <script >  
 
 document.title="ElProfesor"; 
-document.getElementById("foto").src="./images/stalin.png";
+document.getElementById("foto").src="./images/rodriguezgiovanna.jpg";
 document.getElementById("elnombre").innerHTML="El profesor";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
