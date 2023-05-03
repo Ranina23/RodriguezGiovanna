c1eb9dd46cf194c58addcd6635c1ed742aff9052
GiovannaRodriguez <69163589+GiovannaRodriguez@users.noreply.github.com>
2023-05-02 19:59:18 -0500

    guardar

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ document.getElementById("elnombre").innerHTML="Giovanna Sofia Rodríguez Ortiz";
 
  document.getElementById("blog-indice").innerHTML="<p><b>Contenido</b></p><hr> <p><a href='#'>inicio</a></p><p><a href='#blog-seccion2'>Estudios</a></p><p><a href='#blog-seccion3'>Conocimiento informático</a></p><p><a href='#blog-seccion4'>Hooby</a></p>";
 
- document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='eys-kevin.php'>Kevin Aguilar</a></p><p><a href='eys-damaris.php'>Damaris Viallalva</a></p> ";
+ document.getElementById("blog-relacionados").innerHTML="<p><b>Temas relacionados:</b></p><hr> <p><a href='https://congresoutlvte.org/CortezPetter/'>Petter Cortez</a></p><p><a href='https://congresoutlvte.org/OrtizNixon/'>Nixon Ortiz</a></p><p><a href='https://congresoutlvte.org/RamiresMaria/'>Maria Ramires</a></p> ";
  </script>
  
 
